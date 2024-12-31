0ad39943d296598350fc0e38b9005699a4f4dae7
akashrathore94 <akashrathour2011@gmail.com>
2024-12-31 11:47:11 +0530

    star rating comp added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+// import "./index.css";
+// import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />; This movie is
+      rated {movieRating} stars
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Great"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} />
+    <Test />
   </React.StrictMode>
 );
